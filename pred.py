2bb7c774fd2882607c07288e19cda37b065df21a
SRUSHTI PORIWADE <srushtiporiwade02@gmail.com>
2025-07-11 17:28:39 +0530

    Update pred.py

--- pred.py
+++ pred.py
@@ -145,6 +145,7 @@ def predict():
 
     try:
         # Fetch image from the internet
+        headers = {'User-Agent': 'Mozilla/5.0'}
         response = requests.get(image_url, timeout=10)
         response.raise_for_status()  # Raise error if request fails
 
